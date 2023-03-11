f57bf146b7c573e05882319af786483278fef172
Aghogho <aghogho.akpedeye@gmail.com>
2023-03-11 21:53:56 +0200

    designed splashScreen for both light and dark mode

--- app/src/main/java/com/aghogho/borutoanimeapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/aghogho/borutoanimeapp/presentation/screens/splash/SplashScreen.kt
@@ -1,13 +1,16 @@
 package com.aghogho.borutoanimeapp.presentation.screens.splash
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,21 +26,41 @@ fun SplashScreen(navController: NavHostController) {
 
 @Composable
 fun Splash() {
-    Box(
-        modifier = Modifier
-            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_logo),
-            contentDescription = stringResource(R.string.app_logo)
-        )
+    if (isSystemInDarkTheme()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+        }
     }
 }
 
 @Composable
 @Preview
 fun SplashScreenPreview() {
+    Splash()
+}
+
+@Composable
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+fun SplashScreenDarkPreview() {
     Splash()
 }
