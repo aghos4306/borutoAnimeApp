36701214c7454357e8cf463bd8f48b274bb42cfa
Aghogho <aghogho.akpedeye@gmail.com>
2023-03-13 21:13:48 +0200

    onboarding page and welcome screen design

--- app/src/main/java/com/aghogho/borutoanimeapp/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/aghogho/borutoanimeapp/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,14 +1,18 @@
 package com.aghogho.borutoanimeapp.presentation.screens.welcome
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -17,10 +21,7 @@ import androidx.navigation.NavHostController
 import com.aghogho.borutoanimeapp.domain.model.OnBoardingPage
 import com.aghogho.borutoanimeapp.ui.theme.*
 import com.aghogho.borutoanimeapp.util.Constants.ON_BOARDING_PAGE_COUNT
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.HorizontalPagerIndicator
-import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.pager.*
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -56,6 +57,12 @@ fun WelcomeScreen(navController: NavHostController) {
             indicatorWidth = PAGING_INDICATOR_WIDTH,
             spacing = PAGING_INDICATOR_SPACING,
         )
+        FinishButton(
+            modifier = Modifier.weight(1f),
+            pagerState = pagerState
+        ) {
+            
+        }
     }
 }
 
@@ -95,6 +102,36 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
             fontWeight = FontWeight.Medium,
             textAlign = TextAlign.Center
         )
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState: PagerState,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = EXTRA_LARGE_PADDING),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxWidth(),
+            visible = pagerState.currentPage == 2
+        ) {
+            Button(
+                onClick = onClick ,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.buttonBackgroundColor,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Finish")
+            }
+        }
     }
 }
 
